525d9c666940f9ede54404a30631d8a2f1b99f75
2021108102540243 <2021108102540243@lab01-14.ifgoiano.local>
2023-03-28 10:42:48 -0300

    aula dia 28/03

--- cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServelet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServelet.java
@@ -1,6 +1,8 @@
 package br.edu.ifgoiano.controle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,9 +10,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.edu.ifgoiano.entidade.usuario;
+
 @WebServlet("/cadastrarUsuario")
 public class CadastroUsuarioServelet extends HttpServlet {
 
+	//Simular o banco de dados
+	private List<usuario> lstDeUsuario;
+	
+	@Override
+	public void init() throws ServletException {
+		this.lstDeUsuario = new ArrayList<usuario>();
+	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String senha1 = req.getParameter("senha1");
@@ -18,11 +30,29 @@ public class CadastroUsuarioServelet extends HttpServlet {
 		
 		//Verificar se as senhas são iguais
 		if(senha1.equals(senha2)) {
+			usuario usu = new usuario();
+			usu.setNome(req.getParameter("nome"));
+			usu.setEmail(req.getParameter("email"));
+			usu.setSenha(senha1);
+			lstDeUsuario.add(usu);
 			//redirecionar o usuário para a pag. de login
 			resp.sendRedirect("index.html");
 		}else {
+			
 			//redirecionar o usuário para a pag. de cadastro do usuário
 			req.getRequestDispatcher("cadastroUsuario.html").forward(req, resp);
 		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		for (usuario usuario : lstDeUsuario) {
+			System.out.println(usuario.getNome().concat(" - ").concat(usuario.getEmail()));
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		this.lstDeUsuario.clear();
 	}
 }
