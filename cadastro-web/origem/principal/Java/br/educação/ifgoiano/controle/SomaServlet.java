41f482bf250ae543e1fc08370d8daca12740d3be
DudaCostaA <127799214+DudaCostaA@users.noreply.github.com>
2023-04-03 20:10:36 -0300

    Update and rename cadastro-web/src/main/java/br/edu/ifgoiano/controle/SomaServlet.java to cadastro-web/origem/principal/Java/br/educação/ifgoiano/controle/SomaServlet.java

--- cadastro-web/origem/principal/Java/br/educação/ifgoiano/controle/SomaServlet.java
+++ cadastro-web/origem/principal/Java/br/educação/ifgoiano/controle/SomaServlet.java
@@ -1,6 +1,7 @@
 package br.edu.ifgoiano.controle;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,16 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/resultado")
+@WebServlet("/soma")
+
 public class SomaServlet extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String v1 = req.getParameter("v1");
-		String v2 = req.getParameter("v2");
 		
-		String soma = v1 + v2;
+		int nun1 = Integer.parseInt(req.getParameter("n1"));
+		int nun2 = Integer.parseInt(req.getParameter("n2"));
+		
+		
+		int soma = nun1 + nun2;
+
+			
+		resp.getWriter().println("A soma dos valores é " + soma);
+		
+		
 	}
 
 }
