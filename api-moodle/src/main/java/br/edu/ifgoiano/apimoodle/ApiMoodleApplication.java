8189526a03f25a91b0fe891b397852a69004ed43
DudaCostaA <127799214+DudaCostaA@users.noreply.github.com>
2023-10-18 17:00:45 -0300

    Atividade - moodle 3° trimestre
    URL do Github referente à API REST.

---
+++ api-moodle/src/main/java/br/edu/ifgoiano/apimoodle/ApiMoodleApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.apimoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoodleApplication.class, args);
+	}
+
+}
---
+++ api-moodle/src/test/java/br/edu/ifgoiano/apimoodle/ApiMoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.apimoodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
