a55b0fa86c7ce3c6e4093e422e0dc4240eda5299
DudaCostaA <127799214+DudaCostaA@users.noreply.github.com>
2023-10-24 10:35:30 -0300

    aula 24/10
    api medico

---
+++ api/target/classes/static/medico.js
@@ -0,0 +1,1 @@
+{"id":"null","nome":"MAria Eduarda Costa Aguiar","email":"maria.costa@gmail.com","crn":160206,"especialidade":"DERMATOLOGIA","endereco":{"id":null,"logradouro":"Rua Wilson Reis","bairro":"Cristina II","complemento":"Quadra 25 lotes 2","cep":"95548754","cidade":"Trindade","estado":"Goiás"}}
